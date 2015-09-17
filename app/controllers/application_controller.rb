ff65276ff369854c87f7244fccc9eb339cee81aa
Taiki <tyoshikawa1106@gmail.com>
2015-09-17 18:22:18 +0900

    add error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Exception, with: :error500
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+
+  def error404(e)
+    render 'error404', status: 404, formats: [:html]
+  end
+
+  def error500(e)
+    logger.error [e, *e.backtrace].join("\n")
+    render 'error500', status: 500, formats: [:html]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ BootstrapSf1App::Application.routes.draw do
   match '/theme', to: 'samples#theme', via: 'get'
   match '/icons', to: 'samples#icons', via: 'get'
   match '/new', to: 'samples#new', via: 'get'
+  
+  match '*path' => 'application#error404', via: :all
 end
