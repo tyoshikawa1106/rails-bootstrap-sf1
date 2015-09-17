f6a502fbe77483b3e2ce837bb9e2749d3b6cba3d
Taiki <tyoshikawa1106@gmail.com>
2015-09-17 18:32:41 +0900

    add error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from Exception, with: :error500
-  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
-
-  def error404(e)
-    render 'error404', status: 404, formats: [:html]
-  end
 
   def error500(e)
     logger.error [e, *e.backtrace].join("\n")
